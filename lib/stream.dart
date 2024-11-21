e36452e78ea710fbc880d9be1bfe735f1c9b6af0
ersaoktavian <ersaoktavianramadan@gmail.com>
2024-11-22 03:54:26 +0700

    W12: Jawaban Soal 2

---
+++ lib/stream.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.pink,
+    Colors.yellow,
+  ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1),
+      (int t) {
+        int index = t % colors.length;
+        return colors[index];
+      },
+    );
+  }
+}
